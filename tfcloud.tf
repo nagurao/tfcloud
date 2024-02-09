53b168d2dd4892fe388dba54e749b6e88ea53d4b
nagurao <nagurao@users.noreply.github.com>
2024-02-09 16:47:34 +0530

    Update tfcloud.tf

--- tfcloud.tf
+++ tfcloud.tf
@@ -9,6 +9,6 @@ resource "aws_instance" "ec2" {
   ami           = "ami-0a7cf821b91bcccbc"
   instance_type = "t3.medium"
   tags = {
-    Name = "Nagu TF Cloud Test"
+    Name = "Nagu TF Cloud Test Or Jenkins"
   }
 }
